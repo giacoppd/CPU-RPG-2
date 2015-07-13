c3b5f2efc39c6b484b8de747a58303ac1215ca3f
Dominic Giacoppe <giacoppd@onid.orst.edu>
2015-07-13 00:15:10 -0700

    Added TODOs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #TODO
-#Make this happen. For now, dummy shit
+#Make this happen. For now, dummy shit. Combat? Turns? That speed thing?
+#Spells? A bloody interface?
 from cpurpg import *
 
 unitA = unit("A", 1, 0, 100, 5, 5, 5, 100, 5)
@@ -7,6 +8,12 @@ unitB = unit("B", 1, 0, 100, 5, 5, 5, 100, 5)
 unitC = unit("C", 1, 0, 100, 5, 5, 5, 100, 5)
 unitD = unit("D", 1, 0, 100, 5, 5, 5, 100, 5)
 
+baddies1 = unit("Baddies", 1, 0, 10, 2, 2, 2, 10, 4)
+baddies2 = unit("Baddies", 1, 0, 10, 2, 2, 2, 10, 4)
+baddies3 = unit("Baddies", 1, 0, 10, 2, 2, 2, 10, 4)
+baddies4 = unit("Baddies", 1, 0, 10, 2, 2, 2, 10, 4)
+
 userParty = party(unitA, unitB, unitC, unitD)
-#userParty.statquery(userParty.partylist[0])
+baddyParty = party(baddies1, baddies2, baddies3, baddies4)
+
 userParty.statquery(0)
