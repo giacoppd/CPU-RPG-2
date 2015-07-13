d39035fe883c93e5734d1fbd1a8c6ddd08a131a5
Dominic Giacoppe <giacoppd@onid.orst.edu>
2015-07-13 00:09:09 -0700

    Made some things work

--- cpurpg.py
+++ cpurpg.py
@@ -1,12 +1,11 @@
-class spell(object):
+class spell():
     def __init__(self, typ, aoe):
       self.typ = typ #either phys, which is reduced by def + str, magic, reduced by def + int, pure, no reductions, or a non-damage
       #TODO
       #We need to feed it the list of spells joe has. It's going to suck. Just a lot of variety in here.
 
-class unit(object):
-    spell spellList[4]
-    def __init__(self, name, level, xp, vit, STR, INT, DEF, stam, spd, stun, drain, bind, mute):
+class unit():
+    def __init__(self, name, level, xp, vit, STR, INT, DEF, stam, spd):
         self.level = level
 	self.name = name
         self.xp = xp
@@ -34,27 +33,26 @@ class unit(object):
         main[target].sbind -= spelllist[spellselect].sbind
         main[target].smute -= spelllist[spellselect].smute
 
-class party(object):
-	unit partylist[4]
-    def __init__(self, A, B, C, D) #create a party and assign to it
-	self.partylist[0] = A
-	self.partylist[1] = B
-	self.partylist[2] = C
-	self.partylist[3] = D
-    def memSwap(self, slot, newMem) #slot is the position to change, newMem is the replacement char
+class party():
+    def __init__(self, A, B, C, D): #create a party and assign to it
+        self.partylist = []
+        self.partylist.append(A)
+	self.partylist.append(B)
+	self.partylist.append(C)
+	self.partylist.append(D)
+    def memSwap(self, slot, newMem): #slot is the position to change, newMem is the replacement char. Probs should check for already being in party
         self.partylist[slot] = newMem
-    def statquery(targetUnit): #may or may not be useful
+    def statquery(self, num): #may or may not be useful
         form = '{stat:8} {val:8}'
-        name = raw_input ("Enter name of unit: ")
-        print form.format(stat = 'LEVEL', val = targetUnit.level)
-        print form.format(stat = 'XP', val = targetUnit.xp)
-        print form.format(stat = 'VIT', val = targetUnit.vit)
-        print form.format(stat = 'STR', val = targetUnit.STR)
-        print form.format(stat = 'INT', val = targetUnit.INT)
-        print form.format(stat = 'DEF', val = targetUnit.DEF)
-        print form.format(stat = 'STAMINA', val = targetUnit.stam)
-        print form.format(stat = 'SPD', val = targetUnit.spd)
-        print form.format(stat = 'STUN', val = targetUnit.stun)
-        print form.format(stat = 'DRAIN', val = targetUnit.drain)
-        print form.format(stat = 'BIND', val = targetUnit.bind)
-        print form.format(stat = 'MUTE', val = targetUnit.mute)
+        print form.format(stat = 'LEVEL', val = self.partylist[num].level)
+        print form.format(stat = 'XP', val = self.partylist[num].xp)
+        print form.format(stat = 'VIT', val = self.partylist[num].vit)
+        print form.format(stat = 'STR', val = self.partylist[num].STR)
+        print form.format(stat = 'INT', val = self.partylist[num].INT)
+        print form.format(stat = 'DEF', val = self.partylist[num].DEF)
+        print form.format(stat = 'STAMINA', val = self.partylist[num].stam)
+        print form.format(stat = 'SPD', val = self.partylist[num].spd)
+        #print form.format(stat = 'STUN', val = self.partylist[num]stun)
+        #print form.format(stat = 'DRAIN', val = self.partylist[num]drain)
+        #print form.format(stat = 'BIND', val = self.partylist[num]bind)
+        #print form.format(stat = 'MUTE', val = self.partylist[num]mute)
