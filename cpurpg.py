2e9d240a39223d1c74125dad14710143ef3e3d37
Dominic Giacoppe <giacoppd@onid.orst.edu>
2015-07-13 15:45:37 -0700

    Uppercase OP, also added level up stats

--- cpurpg.py
+++ cpurpg.py
@@ -5,29 +5,33 @@ class spell():
       #We need to feed it the list of spells joe has. It's going to suck. Just a lot of variety in here.
 
 class unit():
-    def __init__(self, name, level, xp, vit, STR, INT, DEF, stam, spd):
+    def __init__(self, name, level, xp, VIT, STR, INT, DEF, STAM, SPD, VITup, STRup, INTup, DEFup):
         self.level = level
 	self.name = name
         self.xp = xp
-        self.vit = vit
+        self.VIT = VIT
         self.STR = STR
         self.INT = INT
         self.DEF = DEF
-        self.stam = stam
-        self.spd = spd #if speed is tied, highest current staminia goes first
+        self.STAM = STAM
+        self.SPD = SPD #if speed is tied, highest current STAMinia goes first
         self.stunD = 0 #stun duration, mute, bind, drain
         self.muteD = 0
         self.bindD = 0
         self.drainD = 0
+        self.VITup = VITup
+        self.STRup = STRup
+        self.INTup = INTup
+        self.DEFup = DEFup
     
     def usespell(spellNum): #needs work, maybe just can it for hardcoded spells?
         target = raw_input ("Select target: ")
-        svit -= spelllist[spellselect].svit
+        sVIT -= spelllist[spellselect].sVIT
         sstr -= spelllist[spellselect].sstr
         sint -= spelllist[spellselect].sint
         main[target].sdef -= spelllist[spellselect].sdef
-        main[target].sstam -= spelllist[spellselect].sstam
-        main[target].sspd -= spelllist[spellselect].sspd
+        main[target].sSTAM -= spelllist[spellselect].sSTAM
+        main[target].sSPD -= spelllist[spellselect].sSPD
         main[target].sstun -= spelllist[spellselect].sstun
         main[target].sdrain -= spelllist[spellselect].sdrain
         main[target].sbind -= spelllist[spellselect].sbind
@@ -46,12 +50,12 @@ class party():
         form = '{stat:8} {val:8}'
         print form.format(stat = 'LEVEL', val = self.partylist[num].level)
         print form.format(stat = 'XP', val = self.partylist[num].xp)
-        print form.format(stat = 'VIT', val = self.partylist[num].vit)
+        print form.format(stat = 'VIT', val = self.partylist[num].VIT)
         print form.format(stat = 'STR', val = self.partylist[num].STR)
         print form.format(stat = 'INT', val = self.partylist[num].INT)
         print form.format(stat = 'DEF', val = self.partylist[num].DEF)
-        print form.format(stat = 'STAMINA', val = self.partylist[num].stam)
-        print form.format(stat = 'SPD', val = self.partylist[num].spd)
+        print form.format(stat = 'STAMINA', val = self.partylist[num].STAM)
+        print form.format(stat = 'SPD', val = self.partylist[num].SPD)
         #print form.format(stat = 'STUN', val = self.partylist[num]stun)
         #print form.format(stat = 'DRAIN', val = self.partylist[num]drain)
         #print form.format(stat = 'BIND', val = self.partylist[num]bind)
