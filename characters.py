862eb4c5a497f5cf7b6b8ed56016b6fbc812a900
Dominic Giacoppe <giacoppd@onid.orst.edu>
2015-07-17 15:55:51 -0700

    Added me

--- characters.py
+++ characters.py
@@ -1,6 +1,31 @@
 class Dominic(unit):
     charge
+    def __init__(self):
+    self.charge = 1
     def focus(self):
         charge += 1
-    def gattle(target):
+    def gattle(enemyParty, pos):
+        if (pos == 1):
+             enemyParty[0].HP -= 50*charge
+             enemyParty[1].HP -= 30*charge
+             enemyParty[2].HP -= 30*charge
+             enemyParty[3].HP -= 30*charge
+        else if (pos == 2):
+             enemyParty[1].HP -= 50*charge
+             enemyParty[2].HP -= 30*charge
+             enemyParty[3].HP -= 30*charge
+        else
+             print "How did this happen to me???? Dom gattle"
+    def chen(self, userParty):
+        for p in userParty:
+            p.HP += (50 + self.STR + self.INT)
+            if (p.HP > p.VIT):
+                p.HP = p.VIT
+        self.drainD = 3
+         
+
+
+
+
+
     
