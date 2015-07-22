b6642b5136a7f7d8ea84c2eec07734300581cd07
JJ-Spicket <matthew.bunge@gmail>
2015-07-22 15:26:34 -0700

    Revert "Revert "Blah""
    This reverts commit a843d6cbce4184131bfc27810e007f98df7a4413.

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def instance(userparty, baddyparty):
 				q.put(person)
 		while (q.empty() == false)
 			person = q.get()
-			if (person.stunD == 0):
+			if (person.stunD == 0 and person.HP > 0):
 				action = int(raw_input("What do you want to do?"))
-				#execute action
+				person.takeaction(action, baddyparty, userparty)
 			person.debuffdown()
 			
 		
