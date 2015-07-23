5fe85316e7f347f768aa42b7749d84abf0734d93
Dominic Giacoppe <giacoppd@onid.orst.edu>
2015-07-22 21:18:18 -0700

    Need to fix DEF, changed ailment shit

--- characters.py
+++ characters.py
@@ -7,35 +7,67 @@ class Dom(unit):
     def __init__(self):
         #self, name, level, xp, VIT, STR, INT, DEF, STAM, SPD, VITup, STRup, INTup, DEFup
         super(Dom, self).__init__(self, "Dominic", 1, 0, 90, 5, 5, 5, 10, 4, 10, 2, 2, 2)    
-        self.charge = 1
+        self.charge1 = 0
+        self.charge2 = 0
+    def debuffdown():
+        self.stunPurge()
+	super(Dom, self).debuffdown()
+	if (self.charge1 > 0):
+            self.charge1 -= 1
+	if (self.charge2 > 0):
+            self.charge2 -= 1
+
     def stunPurge(self):
         self.stunD = 0
     
     def focus(self):
-        charge += 1
-        super(Dom, self).useSTAM(6)
-    
+        if (super(Dom, self).checkMute() == True):
+            return False
+        if (self.charge1 == 0):
+            charge1 += 3
+        elif ((self.charge2 == 0) and (self.charge1 == 2)):
+            charge2 += 3
+        elif ((self.charge2 == 2) and (self.charge1 == 1)):
+            print "why you do this, you only get 2 stacks foo"
+            return False
+        else:
+            print "how u do this"
+            return False
+
     def gattle(enemyParty, pos):
+        if (super(Dom, self).checkBind() == True):
+            return False
+        if(super(Dom, self).useSTAM(8) == False):
+            return False
+        if (self.charge1 == 0):
+            amt = 1
+        elif ((self.charge2 == 0) and (self.charge1 == 2)):
+            amt = 2
+        elif ((self.charge2 == 2) and (self.charge1 == 1)):
+            amt = 3
         if (pos == 1):
-             enemyParty[0].HP -= 50*charge
-             enemyParty[1].HP -= 30*charge
-             enemyParty[2].HP -= 30*charge
-             enemyParty[3].HP -= 30*charge
+            enemyParty[0].HP -= 50*amt
+            enemyParty[1].HP -= 30*amt
+            enemyParty[2].HP -= 30*amt
+            enemyParty[3].HP -= 30*amt
         elif (pos == 2):
-             enemyParty[1].HP -= 50*charge
-             enemyParty[2].HP -= 30*charge
-             enemyParty[3].HP -= 30*charge
+            enemyParty[1].HP -= 50*amt
+            enemyParty[2].HP -= 30*amt
+            enemyParty[3].HP -= 30*amt
         else:
              print "How did this happen to me???? Dom gattle"
              return False
-        charge = 1
-        super(Dom, self).useSTAM(8)
+        charge1 = 0
+        charge2 = 0
 
     def chen(self, userParty):
+        if (super(Dom, self).checkMute() == True):
+            return False
+        if(super(Dom, self).useSTAM(10) == False):
+            return False
         for p in userParty:
             p.heal(50 + self.STR + self.INT)
         self.drainD = 3
-        super(Dom, self).useSTAM(10)
         
 
 class Yong(unit):
@@ -45,18 +77,27 @@ class Yong(unit):
         super(Yong, self).__init__(self, "Yongwoon", 1, 0, 150, 10, 6, 0, 10, 5, 30, 3, 1, 0)
     
     def graniteKick(target):
+        if (super(Yong, self).checkBind() == True):
+            return False
+        if(super(Yong, self).useSTAM(10) == False):
+            return False
         target.HP -= (5*self.STR - target.DEF)
-        super(Yong, self).useSTAM(10)
 
     def bloodTrans(partyList):
+        if (super(Yong, self).checkMute() == True):
+            return False
+        if(super(Yong, self).useSTAM(13) == False):
+            return False
         for p in partyList:
             p.gainSTAM(3)
-        super(Yong, self).useSTAM(13)
     
     def bodyTrans(target):
+        if (super(Yong, self).checkMute() == True):
+            return False
+        if(super(Yong, self).useSTAM(5) == False):
+            return False
         self.HP -= 50
         target.heal(5*self.STR)
-        super(Yong, self).useSTAM(5)
 
 class Haley(unit): 
     
@@ -68,22 +109,34 @@ class Haley(unit):
         self.drainD = 0
     
     def unravel(self):
+        if (super(Haley, self).checkMute() == True):
+            return False
+        if(super(Haley, self).useSTAM(3) == False):
+            return False
         self.bindD = 0
-        super(Haley, self).useSTAM(3)
     
     def dash(self, enemyParty):
+        if (super(Haley, self).checkBind() == True):
+            return False
+        if(super(Haley, self).useSTAM(5) == False):
+            return False
         for e in enemyParty:
             e.HP -= (2*self.STR + 10 - e.DEF)    
-        super(Haley, self).useSTAM(5)
     
     def mountainChisel(self, target):
+        if (super(Haley, self).checkBind() == True):
+            return False
+        if(super(Haley, self).useSTAM(10) == False):
+            return False
         target.HP -= (4*(3*self.STR + 15) - target.DEF)
-        super(Haley, self).useSTAM(10)
     
     def whirlKick(self, enemyParty):
+        if (super(Haley, self).checkBind() == True):
+            return False
+        if(super(Haley, self).useSTAM(3) == False):
+            return False
         enemyParty[0].HP -= (2*self.STR + 15 - enemyParty[0].DEF)    
         enemyParty[1].HP -= (2*self.STR + 15 - enemyParty[1].DEF)    
-        super(Haley, self).useSTAM(3)
 
 class Nick(unit):
    
@@ -92,25 +145,32 @@ class Nick(unit):
         super(Nick, self).__init__(self, "Nick", 1, 0, 60, 5, 6, 5, 10, 6, 10, 1, 1, 1)
     
     def takeaction(self, i): #nick has double move, is self even needed idk I wish I could compile
-            super(Nick, self).takeaction(i)
-            #TODO reprompt for input here
-            #i = input()
-            super(Nick, self).takeaction(i)
+        super(Nick, self).takeaction(i)
+        #TODO reprompt for input here
+        #i = input()
+        super(Nick, self).takeaction(i)
     
     def blitz(self, target):
+        if (super(Nick, self).checkBind() == True):
+            return False
+        if(super(Nick, self).useSTAM(4) == False):
+            return False
         target.HP -= ((self.STR + 30) - target.DEF)
-        super(Nick, self).useSTAM(4)
     
     def rally(self, target):
+        if (super(Nick, self).checkMute() == True):
+            return False
+        if(super(Nick, self).useSTAM(4) == False):
+            return False
         target.gainSTAM(4)
-        super(Nick, self).useSTAM(4)
     
     def stretch(self):
+        if(super(Nick, self).useSTAM(10) == False):
+            return False
         self.stunD = 0
         self.bindD = 0
         self.drainD = 0
         self.muteD = 0
-        super(Nick, self).useSTAM(10)
 
 class Kath(unit):
 
@@ -120,20 +180,28 @@ class Kath(unit):
         self.fort = False
 
     def totem(self, enemyParty):
+        if (super(Kath, self).useSTAM(10) == False):
+            return False
         for e in enemyParty:
-            e.stunD += 2
+            e.stunD = 2
         self.stunD += 2
         self.drainD += 2
-        super(Kath, self).useSTAM(10)
 
     def obliteration(self, enemyParty):
+        if (super(Kath, self).checkMute() == True):
+            return False
+        if (super(Kath, self).useSTAM(10) == False):
+            return False
         DMG = (self.HP // 2) #damage is floored with the //
         for e in enemyParty:
             e.HP -= DMG
         self.HP -= DMG
-        super(Kath, self).useSTAM(10)
     
     def uniPurge(self, userParty):
+        if (super(Kath, self).checkMute() == True):
+            return False
+        if (super(Kath, self).useSTAM(10) == False):
+            return False
         for u in userParty:
             u.stunD = 0
             u.bindD = 0
@@ -141,10 +209,10 @@ class Kath(unit):
             u.drainD = 0
         self.drainD = 9999
         self.stunD = 9999 #big numbers so she's down for the count
-        super(Kath, self).useSTAM(10)
    
-   def fortressT(self): #TODO ask joe about how to toggle and if can be purged
-        super(Kath, self).useSTAM(6)
+   def fortressT(self): #TODO toggle takes 1 turn and no stam
+        if (super(Kath, self).useSTAM(6) == False):
+            return False
         if(self.fort == False):
            self.fort == True
            self.bindD = 9999
@@ -161,20 +229,32 @@ class JennyK(unit):
         self.block = False
     
     def block(self):
+        if (super(JennyK, self).checkBind) == True):
+            return False
+        if(super(JennyK, self).useSTAM(3) == False):
+            return False
         self.block = True
-        super(JennyK, self).useSTAM(3)
     
     def piercingKick(self, target):
+        if (super(JennyK, self).checkBind) == True):
+            return False
+        if(super(JennyK, self).useSTAM(4) == False):
+            return False
         target.HP -= ((2*self.STR + 2*self.INT) - target.DEF) #Row reduction?
-        super(JennyK, self).useSTAM(4)
 
     def ROTCDrill(self, tiarget):
+        if (super(JennyK, self).checkBind) == True):
+            return False
+        if(super(JennyK, self).useSTAM(5) == False):
+            return False
         target.gainSTAM(5)
-        super(JennyK, self).useSTAM(5)
     
     def artOfDeath(self, target):
+        if (super(JennyK, self).checkMute() == True):
+            return False
+        if(super(JennyK, self).useSTAM(10) == False):
+            return False
         target.HP -= (target.VIT - target.HP)
-        super(JennyK, self).useSTAM(10)
 
 class JennyC(unit):
     
@@ -185,24 +265,33 @@ class JennyC(unit):
         self.embossD = 0
     
     def debuffdown():
-		super(JennyC, self).debuffdown()
-		if (embossD > 0):
-			embossD -= 1
-			if(embossD == 0):
-			    return "emON"
+	super(JennyC, self).debuffdown()
+	if (embossD > 0):
+	    embossD -= 1
+	    if(embossD == 0):
+		return "emOFF"
 	
     def vitalize(self, userParty):
+        if (super(JennyC, self).checkMute() == True):
+            return False
+        if (super(JennyC, self).useSTAM(2) == False):
+            return False
         for u in userParty:
             u.heal(self.INT + 5)
-        super(JennyC, self).useSTAM(2)
 
     def invigorate(self, userParty):
+        if (super(JennyC, self).checkMute() == True):
+            return False
+        if (super(JennyC, self).useSTAM(3) == False):
+            return False
         for u in userParty:
             u.gainSTAM(1)
-        super(JennyC, self).useSTAM(3)
 
     def remove(self, target, choice):
-        super(JennyC, self).useSTAM(7)
+        if (super(JennyC, self).checkMute() == True):
+            return False
+        if (super(JennyC, self).useSTAM(7) == False):
+            return False
         if(choice == 1):
             target.stunD = 0
         if(choice == 2):
@@ -216,9 +305,12 @@ class JennyC(unit):
             return False
 
     def emboss(self, userParty): 
+        if (super(JennyC, self).checkMute() == True):
+            return False
+        if (super(JennyC, self).useSTAM(10) == False):
+            return False
         for u in userParty:
             u.DEF += self.DEF
-        super(JennyC, self).useSTAM(10)
         return "emON"
 
 class Natasha(unit):
@@ -230,32 +322,44 @@ class Natasha(unit):
         self.qStack = 0
     
     def debuffdown():
-		super(Natasha, self).debuffdown()
-		if (qD > 0):
-			qD -= 1
-			if(qD == 0):
-			    self.DEF -= self.qStack*2
-                self.qStack = 0
+	super(Natasha, self).debuffdown()
+	    if (qD > 0):
+		qD -= 1
+		if(qD == 0):
+		    self.DEF -= self.qStack*2
+                    self.qStack = 0
     
     def qigong(self):
-            self.DEF += 2
-            self.qStack += 1
-            self.qD += 3
-            super(Natasha, self).useSTAM(3)
+        if (super(Natasha, self).checkBind) == True):
+            return False
+        if(super(Natasha, self).useSTAM(3) == False):
+            return False
+        self.DEF += 2
+        self.qStack += 1
+        self.qD += 3
 
     def unrefinedKick(self, target):
+        if (super(Natasha, self).checkBind) == True):
+            return False
+        if(super(Natasha, self).useSTAM(3) == False):
+            return False
         amount = ((3*self.STR + 10) - target.DEF) 
         target.HP -= amount
         self.HP -= amount // 2
-        super(Natasha, self).useSTAM(3)
 
     def gungirFlourish(self, target):
+        if (super(Natasha, self).checkBind) == True):
+            return False
+        if(super(Natasha, self).useSTAM(8) == False):
+            return False
         target.HP -= ((6*self.STR + 30) - target.DEF) 
-        super(Natasha, self).useSTAM(8)
     
     def gungirCombo(self, target):
+        if (super(Natasha, self).checkBind) == True):
+            return False
+        if(super(Natasha, self).useSTAM(10) == False):
+            return False
         target.HP -= ((8*(2*self.STR + 10) - target.DEF))
-        super(Natasha, self).useSTAM(10)
 
 class Ulri(unit):
    
@@ -265,26 +369,35 @@ class Ulri(unit):
         self.phD = 0 #phalanx duration
     
     def debuffdown():
-		super(Ulri, self).debuffdown()
-		if (phD > 0):
-			phD -= 1
-			if(phD == 0):
-			    self.DEF -= DEF
+	super(Ulri, self).debuffdown()
+	if (phD > 0):
+	    phD -= 1
+	    if(phD == 0):
+	        self.DEF -= DEF
 			
     def trueStrike(self, target):
+        if (super(Ulri, self).checkBind) == True):
+            return False
+        if(super(Ulri, self).useSTAM(5) == False):
+            return False
         target.HP -= 2*(self.STR + self.INT)
-        super(Ulri, self).useSTAM(5)
 
     def phalanx(self, target): 
-        target.bindD += 4
+        if (super(Ulri, self).checkBind) == True):
+            return False
+        if(super(Ulri, self).useSTAM(5) == False):
+            return False
+        target.bindD = 4
         self.DEF += 10
         self.phD = 4
-        super(Ulri, self).useSTAM(5)
         
     def oblation(self, userParty):   
+        if(super(Ulri, self).useSTAM(10) == False):
+            return False
+        if (super(Ulri, self).checkMute() == True):
+            return False
         for u in userParty:
             u.gainSTAM(10)
-        super(Ulri, self).useSTAM(10)
         self.HP -= (self.VIT * .7) // 1 #cheater's floor func, why import when you can just //?
 
 class Morgan(unit):
@@ -304,15 +417,69 @@ class Morgan(unit):
                 #Bind immune?
     
     def danceKnives(self, target):
+        if (super(Morgan, self).checkBind) == True):
+            return False
+        if (super(Morgan, self).useSTAM(5) == False):
         target.HP -= (5*(self.STR + 10) - target.DEF)
-        super(Morgan, self).useSTAM(5)
     
     def danceDazzle(self, enemyParty):
+        if (super(Morgan, self).checkBind) == True):
+            return False
+        if (super(Morgan, self).useSTAM(5) == False):
+            return False
         for e in enemyParty:
             e.HP -= 40
         enemyParty[0].stunD = 1
-        super(Morgan, self).useSTAM(5)
+
     def danceDeath(self, enemyParty, userParty, roster):
-            #TODO do we check for rachel in here or somewhere else?
+        if (super(Morgan, self).checkBind) == True):
+            return False
+        if (self.duo == False):
+            return False
+        if (super(Morgan, self).useSTAM(10) == False):
+            return False
+        for (e in enemyParty):
+            e.HP -= (3*self.STR + 50) - (e.DEF + e.STR)
+        
+
+
+class Rachel(unit):
+   
+    def __init__(self):
+        #self, name,  level, xp,VIT,STR,INT,DEF,STAM,SPD,VITup, STRup, INTup, DEFup
+        super(Rachel, self).__init__(self, "Rachel", 1, 0, 80, 8, 7, 3, 10, 4, 15, 2, 2, 2)
+        #TODO finish these stupid sisters when I have proper swaping
+
+
+class Chris(unit):
+   
+    def __init__(self):
+        #self, name,  level, xp,VIT,STR,INT,DEF,STAM,SPD,VITup, STRup, INTup, DEFup
+        super(Chris, self).__init__(self, "Christine", 1, 0, 70, 6, 8, 6, 10, 3, 5, 1, 2, 1)
+    
+    def analyze(self, target):
+        if (super(Chris, self).useSTAM(5) == False):
+            return False
+        if (super(Chris, self).checkMute() == True):
+            return False
+        target.analyzeB = True
+
+    def cripple(self, target):
+        if (super(Chris, self).useSTAM(8) == False):
+            return False
+        if (super(Chris, self).checkMute() == True):
+            return False
+        target.HP -= (4*self.INT + 20 - target.DEF - target.INT)
+        target.bindD = 2
+        target.muteD = 2
+
+    def deepPurge(self, userParty):
+        if (super(Chris, self).useSTAM(8) == False):
+            return False
+        for u in userParty:
+            u.muteD = 0
+        
+
+        
 
 
