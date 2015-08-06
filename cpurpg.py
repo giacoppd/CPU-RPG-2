f89d4a80944331386026195c1211fe6442023c30
Dominic Giacoppe <giacoppd@onid.orst.edu>
2015-08-06 14:44:28 -0700

    More indentation fixes

--- cpurpg.py
+++ cpurpg.py
@@ -1,5 +1,3 @@
-
-
 class unit():    
     embossAmount = 0 #emboss amount, because it's easier than passing the party to get Jenny's DEF. It's static
     def __init__(self, name, level, xp, VIT, STR, INT, DEF, STAM, SPD, VITup, STRup, INTup, DEFup):
@@ -41,27 +39,29 @@ class unit():
 	if self.STAM < amount:
 	    print("not enough stamina to use ability")
 	    return False
-	self.STAM -= amount
+        else:
+    	self.STAM -= amount
 	    return True
 
     def gainSTAM(self, amount):
 	if self.drainD == 0:
 	    self.STAM += amount
-		if (self.STAM > 10):
-		    self.STAM = 10    
-		    return True
+            if (self.STAM > 10):
+                self.STAM = 10    
+		return True
         else:
 	    print("Failed: You are Drained")
-	        return False
+	    return False
     
     def heal(self, amount):
         if(self.HP <= 0):
             print "Can't heal outta death"
             return False
+
         self.HP += amount
         if (self.HP > self.VIT):
             self.HP = self.VIT  
-		return True
+	    return True
     
     def levelUp(self):
         self.level += 1
@@ -70,51 +70,52 @@ class unit():
         self.STR += STRup
         self.INT += INTup
         self.DEF += DEFup 
-	def attack(self, target):
-		if not checkBind():
-			if self.STR - target.DEF > 0
-				target.HP -= (self.STR - target.DEF)
-			return True
-		return False
-	def debuffdown(self):
-		if self.bindD > 0:
-		    self.bindD -= 1
-		if self.stunD > 0:
-		    self.stunD -= 1
-		if self.drainD > 0:
-		    self.drainD -= 1
-		if self.muteD > 0:
-	            self.muteD -= 1
-                if self.barrD > 0: #Kenji's barrier
-                    self.barrD -= 1
-                    if (self.barrD == 0):
-                        self.DEF -= 5
-                if self.embossD > 0: #Jenny's def buff
-                    self.embossD -= 1
-                    if (self.embossD == 0):
-                        self.DEF -= embossAmount
 
-	def takeaction(self, i, baddyparty, userparty):
-		if i == 1:
-			Nexecuted = True
-			while(Nexecuted):
-				target = int(raw_input("Who do you want to attack? "))
-				if target == 1 or target == 2:
-					attack(baddyparty[i - 1])
-					Nexecuted = False;
-				else:
-					print("Invalid target")
-					
+    def attack(self, target):
+        if (checkBind() != True):
+	    if (self.STR - target.DEF > 0):
+		target.HP -= (self.STR - target.DEF)
+		return True
+            else:
+	        return False
 
+    def debuffdown(self):
+        if self.bindD > 0:
+            self.bindD -= 1
+        if self.stunD > 0:
+            self.stunD -= 1	
+        if self.drainD > 0:
+            self.drainD -= 1
+        if self.muteD > 0:
+            self.muteD -= 1
+        if self.barrD > 0: #Kenji's barrier
+            self.barrD -= 1
+            if (self.barrD == 0):
+                self.DEF -= 5
+        if self.embossD > 0: #Jenny's def buff
+            self.embossD -= 1
+            if (self.embossD == 0):
+                self.DEF -= embossAmount
+
+        def takeaction(self, i, baddyparty, userparty):
+            if i == 1:
+                Nexecuted = True
+                while(Nexecuted):
+                    target = int(raw_input("Who do you want to attack? "))
+                    if ((target == 1) or (target == 2)):
+                        attack(baddyparty[i - 1])
+                        Nexecuted = False;
+                    else:
+                        print("Invalid target")
 
 class party():
 
     def __init__(self, A, B, C, D): #create a party and assign to it
         self.partylist = []
         self.partylist.append(A)
-	self.partylist.append(B)
-	self.partylist.append(C)
-	self.partylist.append(D)
+        self.partylist.append(B)
+        self.partylist.append(C)
+        self.partylist.append(D)
 
     def memSwap(self, slot, roster, rosterSlot): #slot is the position to change, roster is the overall player roster, rosterSlot is their location in the roster
         unit dummy = self.partylist[slot]
